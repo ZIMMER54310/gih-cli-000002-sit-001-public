10f292c1f1a81046bdbc8b8f28c7ff4d0c9796b7
ZIMMER54310 <pascal.zimmer@demainsite.fr>
2026-08-14 08:27:12 +0200

    Add files via upload

---
+++ src/data/siteConfig.js
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  clientCode: 'CLI-000002',
+  siteCode: 'SIT-001',
+  displayName: "Acti'Dem",
+  domain: 'actidem.fr',
+  azureStaticWebApp: 'dse-cli-000002-sit-001-public',
+  githubRepository: 'gih-cli-000002-sit-001-public',
+  source: 'SharePoint',
+  privatePortal: 'Power Pages',
+  slogan: "Agir, accompagner, remettre de l'ordre avec humanite",
+  contactEmail: 'a-completer@actidem.fr',
+  contactPhone: 'A completer'
+};
+export const pages = ['Accueil','Syndrome de Diogene','Debarras','Brocante d Automne 2026','Contact'];
